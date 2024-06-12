fa07564e363da672b74393f771028cc94b283028
romant04 <roman.tarnai.04@gmail.com>
2024-06-12 18:29:33 +0200

    registration testing

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,3 +1,7 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
 describe("Votify register", () => {
   const usedEmail = "roman.tarnai.04@gmail.com";
   const email = chance.email();
@@ -18,5 +22,71 @@ describe("Votify register", () => {
     // links
     cy.get('a[href="/cs?"]').should("exist");
   });
+
+  it("should not allow blank email", () => {
+    cy.get('button[type="submit"]:visible').click();
+    cy.get('button[type="submit"]:visible').click(0, 50, { force: true });
+    cy.contains("Povinné").should("exist");
+  });
+
+  it("should not allow invalid email", () => {
+    cy.get('input[name="email"]').type("invalid-email");
+    cy.get('button[type="submit"]:visible').click();
+    cy.contains("Nevalidní e-mail").should("exist");
+  });
+
+  it("should not allow blank password", () => {
+    // Fill email first
+    cy.get('input[name="email"]').type(email);
+
+    cy.get('button[type="submit"]:visible').click();
+    cy.get('button[type="submit"]:visible').click(0, 50, { force: true });
+    cy.contains("Povinné").should("exist");
+  });
+
+  it("should not allow blank password confirmation", () => {
+    // Fill email and password first
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+
+    cy.get('button[type="submit"]:visible').click();
+    cy.get('button[type="submit"]:visible').click(0, 50, { force: true });
+    cy.contains("Povinné").should("exist");
+  });
+
+  it("should not allow weak password", () => {
+    // Fill email first
+    cy.get('input[name="email"]').type(email);
+
+    cy.get('input[name="password"]').type("heslo");
+    cy.get('button[type="submit"]:visible').click();
+    cy.contains("Musí obsahovat alespoň 8 znaků").should("exist");
+  });
+
+  it("should not allow different password confirmation", () => {
+    // Fill email and password first
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+
+    cy.get('input[name="passwordConfirm"]').type("different-password");
+    cy.get('button[type="submit"]:visible').click();
+    cy.contains("Kontrola hesla se neshoduje").should("exist");
+  });
+
+  it("should not allow used email", () => {
+    cy.get('input[name="email"]').type(usedEmail);
+    cy.get('input[name="password"]').type(password);
+    cy.get('input[name="passwordConfirm"]').type(password);
+    cy.get('button[type="submit"]:visible').click();
+    cy.contains("Tento e-mail již někdo používá").should("exist");
+  });
+
+  it("should register successfully", () => {
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get('input[name="passwordConfirm"]').type(password);
+    cy.get('button[type="submit"]:visible').click();
+
+    cy.url().should("include", "successful-sign-up");
+  });
 });
-// successful-sign-up
